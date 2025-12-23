bec57e193dceb8fc4359745ce5b2887513821c83
bspb74 <bspb74@gmail.com>
2025-12-23 15:34:36 -0600

    disabling debug reloader

--- spb_website/app.py
+++ spb_website/app.py
@@ -97,4 +97,4 @@ def check_conn(app):
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
--- spb_website/auth.py
+++ spb_website/auth.py
@@ -5,8 +5,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
-
-
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     session.permanent = True
